73ef8f443e43ca63d591f7c903d94b973d167a9c
Connor Grady <cgrady17@outlook.com>
2016-04-02 00:21:24 -0400

    Initial implementation

--- ForkPipe.c
+++ ForkPipe.c
@@ -7,10 +7,12 @@
 int main() {
 
     // Child process ids
-    int c1, c2;
+    int c1, c2, data;
 
     // Pipe arrays
     int filedes1[2], filedes2[2];
+
+    FILE *fd;
 
     // Create Pipe 1
     if (pipe(filedes1)) {
@@ -26,17 +28,77 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    /* Create Child 1 */
+    // Create Child 1
     c1 = fork();
     if (c1 < 0) {
         // Fork failed
         fprintf(stderr, "Fork failed.\n");
         return EXIT_FAILURE;
-    }
+    } else if (c1 == 0) {
+        // CHILD 1
+        // Output the process id
+        printf("Child 1 PID: %d\n", getpid());
 
-    c2 = fork();
-    if (c2 < 0) {
+        // Open a file for writing
+        fd = fopen("c1file", "w");
+        close(filedes1[0]);
+        close(filedes2[0]);
+
+        for (int i = 0; i < 25; i++) {
+            // Read data from filedes1
+            read(filedes1[0], (char *)&data, sizeof(int));
+
+            // Output value of data
+            printf("Received %d\n", data);
+        }
+
+        return EXIT_SUCCESS;
+    } else if ((c2 = fork()) < 0) {
         fprintf(stderr, "Fork failed.\n");
         return EXIT_FAILURE;
+    } else if (c2 == 0) {
+        // CHILD 2
+        printf("Child 2 PID: %d\n", getpid());
+        close(filedes1[0]);
+        close(filedes2[0]);
+
+        // Open the file for writing
+        fd = fopen("c2file", "w");
+
+        // Loop for 25 times
+        for (int i = 0; i < 25; i++) {
+            // Read data from filedes2
+            read(filedes2[0], (char *)&data, sizeof(int));
+            // Output value of data being received
+            fprintf(fd, "Received %d\n", data);
+            // Incrememt data by 5
+            data = data + 5;
+            // Output value of data being sent
+            fprintf(fd, "Sending %d\n", data);
+            // Write data to filedes1
+            write(filedes1[1], (void *)&data, sizeof(int));
+        }
+
+        // Exit the program
+        return EXIT_SUCCESS;
+    }
+
+    // PARENT PROCESS
+    // Output process id
+    printf("Parent PID: %d\n", getpid());
+    // Open file "pfile" for writing
+    fd = fopen("pfile", "w");
+
+    // Close other ends of pipes
+    close(filedes1[0]);
+    close(filedes2[0]);
+
+    // Loop 25 times
+    for (int i = 0; i < 25; i++) {
+        // Output value of i
+        printf("Sending %d\n", i);
+
+        // Write value of i
+        write(filedes2[1], (void *)&i, sizeof(int));
     }
 }
