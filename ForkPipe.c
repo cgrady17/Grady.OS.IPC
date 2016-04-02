e23e210dfd4a9c81b8ad012398d0d2f971b2de36
Connor Grady <cgrady17@outlook.com>
2016-04-01 23:34:12 -0400

    Forks and pipes created

--- ForkPipe.c
+++ ForkPipe.c
@@ -2,13 +2,41 @@
 #include <limits.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main() {
+
+    // Child process ids
     int c1, c2;
 
-    printf("I am child1");
+    // Pipe arrays
+    int filedes1[2], filedes2[2];
 
+    // Create Pipe 1
+    if (pipe(filedes1)) {
+        // Pipe failed
+        fprintf(stderr, "Pipe failed.\n");
+        return EXIT_FAILURE;
+    }
+
+    // Create Pipe 2
+    if (pipe(filedes2)) {
+        // Pipe failed
+        fprintf(stderr, "Pipe failed.\n");
+        return EXIT_FAILURE;
+    }
+
+    /* Create Child 1 */
     c1 = fork();
+    if (c1 < 0) {
+        // Fork failed
+        fprintf(stderr, "Fork failed.\n");
+        return EXIT_FAILURE;
+    }
 
-    printf("I have been forked: %d", c1);
+    c2 = fork();
+    if (c2 < 0) {
+        fprintf(stderr, "Fork failed.\n");
+        return EXIT_FAILURE;
+    }
 }
